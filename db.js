263a6b1896c4f2a058d4452af71bedf7a050f1c2
Ravi-nanda-bala <ruderavi1998@gmail.com>
2021-12-08 21:21:42 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect('mongodb+srv://ravi:ravi1998@cluster0.eylp9.mongodb.net/shop?retryWrites=true&w=majority' , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
